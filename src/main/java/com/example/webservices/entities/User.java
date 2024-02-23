13c669f7a8d28aec910fbd81cb2408ef7fdcdb5c
Iago <iagomauricio7@gmail.com>
2024-02-23 10:29:21 -0300

    Initial commit

---
+++ src/main/java/com/example/webservices/entities/User.java
@@ -0,0 +1,11 @@
+package com.example.webservices.entities;
+
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+
+}
