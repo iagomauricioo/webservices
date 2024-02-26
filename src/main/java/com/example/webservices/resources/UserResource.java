9785c95382da2a69874e0ef55ea40c05768dcd6c
Iago <iagomauricio7@gmail.com>
2024-02-26 09:02:22 -0300

    Update UserResource for GetMapping

--- src/main/java/com/example/webservices/resources/UserResource.java
+++ src/main/java/com/example/webservices/resources/UserResource.java
@@ -2,14 +2,17 @@ package com.example.webservices.resources;
 
 import com.example.webservices.entities.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
     public ResponseEntity<User> findAll() {
-        User user = new User(1, "Iago", "iago@email.com", "8299999999", "12345");
+        User user = new User(1L, "Iago", "iago@email.com", "8299999999", "12345");
         return ResponseEntity.ok().body(user);
     }
 }
