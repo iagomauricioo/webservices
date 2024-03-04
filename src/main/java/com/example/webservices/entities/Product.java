79b0f79a4c6103aef24c1d13cc4acc25e05ed5aa
Iago Mauricio <iagomauricio7@gmail.com>
2024-03-04 07:07:36 -0300

    product and category manytomany

--- src/main/java/com/example/webservices/entities/Product.java
+++ src/main/java/com/example/webservices/entities/Product.java
@@ -3,55 +3,51 @@ package com.example.webservices.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
 
-    @ManyToMany
-    private List<Product> products;
+    @OneToMany
+    private Set<Category> categories = new HashSet<>();
 
-    public Category() {
+    public Product() {
     }
 
-    public Category(Long id, String name) {
+    public Product(Integer id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id);
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
